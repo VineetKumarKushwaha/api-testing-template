746d7cc26d3899808464cb44116f225bcb9d4a8a
vineet kumar <javascripter.pune@outlook.com>
2021-07-17 11:01:10 +0100

    Update auth.js

--- src/origins/localhost/auth.js
+++ src/origins/localhost/auth.js
@@ -2,3 +2,8 @@ const ModuleKey = require("./index");
 const sessionUtils = require("../../utils/index");
 
 module.exports = sessionUtils(ModuleKey);
+
+var accountName = "sddfsdfsdfs";
+var account_name = "sddsf";
+var password = "sddfdssdfsdf"
+var username = "dfdfsfsdf"
